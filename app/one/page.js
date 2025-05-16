66f73d728f3495d0a83277775d2a4491b9036f3a
Tushar Debnath <endersmoon@gmail.com>
2025-05-16 09:52:05 +0530

    changes

--- app/one/page.js
+++ app/one/page.js
@@ -6,14 +6,14 @@ import {
   User,
   Calendar,
   CreditCard,
+  ArrowLeftIcon,
 } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { BasicForm } from './components/BasicForm';
 import JobInfo from './components/JobInfo';
 import CandidateInfo from './components/CandidateInfo';
 import BaseInfo from './components/BaseInfo';
-
-
+import { Button } from '@/components/ui/button';
 
 const tabs = [
   {
@@ -48,30 +48,46 @@ export default function One() {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div>
-      <div className='border-b flex  items-center overflow-x-auto bg-background sticky top-0 z-10 '>
-        {tabs.map((tab, index) => (
-          <div
-            key={index}
-            onClick={() => setActiveTab(index)}
-            className={cn(
-              'flex flex-1 items-center h-18 cursor-pointer  z-50',
-              index < tabs.length - 1 && 'border-r',
-              activeTab === index && 'border-b-2 border-b-primary'
-            )}>
-            <div className='hidden w-10 h-10 ml-3 rounded-md bg-muted md:flex items-center justify-center'>
-              <tab.icon className='w-6 h-6 ' />
-            </div>
+    <div className='flex flex-col h-screen justify-between'>
+      <div>
+        <div className='border-b flex  items-center overflow-x-auto bg-background sticky top-0 z-10 '>
+          {tabs.map((tab, index) => (
+            <div
+              key={index}
+              onClick={() => setActiveTab(index)}
+              className={cn(
+                'flex flex-1 items-center h-18 cursor-pointer  z-50',
+                index < tabs.length - 1 && 'border-r',
+                activeTab === index && 'border-b-2 border-b-primary'
+              )}>
+              <div className='hidden w-10 h-10 ml-3 rounded-md bg-muted md:flex items-center justify-center'>
+                <tab.icon className='w-6 h-6 ' />
+              </div>
 
-            <div className='ml-3'>
-              <p className='text-xs text-muted-foreground'>Step {index + 1}</p>
-              <p className='text-sm font-medium'>{tab.name}</p>
+              <div className='ml-3'>
+                <p className='text-xs text-muted-foreground'>
+                  Step {index + 1}
+                </p>
+                <p className='text-sm font-medium'>{tab.name}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+
+        <div className='mx-auto max-w-[720px] p-4 overflow-hidden scroll-auto'>
+          {tabs[activeTab].content}
+        </div>
       </div>
-
-      <div className='mx-auto max-w-[720px] p-4 overflow-hidden scroll-auto'>{tabs[activeTab].content}</div>
+      <div className=' w-full h-18 bg-background border-t flex items-center justify-between px-6'>
+        <div className='flex items-center'>
+          <Button variant='outline'>
+            Reset
+          </Button>
+        </div>
+        <Button variant='outline'>
+            Save & Continue
+          </Button>
+      </div>
     </div>
   );
 }
